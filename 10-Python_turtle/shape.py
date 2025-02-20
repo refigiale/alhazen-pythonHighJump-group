e2c9081275b13b8539f7a6cc97aa41423b3ca2d2
refigiale <refinaanindalegia@gmail.com>
2025-02-20 20:17:44 +0700

    10 python_turtlr

---
+++ 10-Python_turtle/shape.py
@@ -0,0 +1,15 @@
+from turtle import *
+
+showturtle()
+getscreen()
+bgcolor("yellow")
+begin_fill
+for i in range(4):
+    forward(50)
+    left(90)
+end_fill
+
+
+fillcolor("red")
+shape("square") #membuat bentuk tanda panah
+done()
